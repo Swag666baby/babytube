a0378558d2c62d59831ed0ca1c7c8bf605d5641b
Baby <89675173+Swag666baby@users.noreply.github.com>
2023-05-18 15:10:47 -0300

    Add files via upload

--- src/videoDownload.py
+++ src/videoDownload.py
@@ -1,6 +1,7 @@
 from pytube import YouTube
 import sys
 link = sys.argv[1]
+file = sys.argv[2]
 yt = YouTube(link)
 stream = yt.streams.get_highest_resolution()
-stream.download()
+stream.download(filename=str(file))
